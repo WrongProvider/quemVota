b50fa38b580fa03e14bfee50e69144af476fd083
Sucuri Security <guara.taste@gmail.com>
2026-02-07 07:23:51 -0300

    install npm + dependencias de react + tailwind + vite

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from backend.routers import politico
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Quem Vota API",
@@ -10,3 +11,22 @@ app = FastAPI(
 app.include_router(politico.router)
 # app.include_router(evento.router)
 # app.include_router(orgao.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["GET,HEAD"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {
+        "status": "ok",
+        "api": "Quem Vota",
+        "docs": "/docs"
+    }
