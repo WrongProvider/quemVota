91ed4a132de8298ad4b5bd000053b6dfc87e8123
Sucuri Security <guara.taste@gmail.com>
2026-04-07 10:35:42 -0300

    correction on cd

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    VALKEY_URL: str = "redis://localhost:6379"
+    ALLOWED_ORIGINS: list[str] = ["http://localhost:5173","http://localhost:3000"]
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
